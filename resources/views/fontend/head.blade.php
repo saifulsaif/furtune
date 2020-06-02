49a9ad1a237c54cc9dfe516647d9769121af0373
Md Sajib Mulla <sajib155@gmail.com>
2020-06-03 00:23:55 +0600

    Last

--- resources/views/fontend/head.blade.php
+++ resources/views/fontend/head.blade.php
@@ -7,19 +7,22 @@
 
 <title>Fortune</title>
 <meta name="description" content="FortuneDiagnostics is the best diagnostic centre in Kolkata for complete health checkups, blood test, cardiology, Pathology, x-ray, Usg, CT scan, ECG"/>
-<meta name="keywords" content="Best Diagnostic Clinic in Kolkata"/>
+<meta name="keywords" content=""/>
 <link rel="canonical" href="index.html" />
 
 
-<!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-121338201-1"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-121338201-1');</script><link rel="icon" type="image/png" href="images/fav-icon.png">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121338201-1"></script>
+<script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-121338201-1');</script>
+{{-- <link rel="icon" type="image/png" href="images/fav-icon.png"> --}}
 
 
 
- <link rel="icon" type="image/png" href="{{ asset('public/images/furtune.JPG') }}">
-   <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link rel="icon" type="image/png" href="{{ asset('public/images/furtune.JPG') }}">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Arimo:400,700" rel="stylesheet">
- <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
 
 <link rel="stylesheet" href="{!! asset('public/css/suraksha8e0e.css?v=8') !!}" type="text/css" />
 
@@ -51,7 +54,7 @@ if (isMobile) {
 
 <style>
     select{
-     color:black !important;
+        color:black !important;
     }
     table.dataTable thead .sorting:after,
     table.dataTable thead .sorting:before,
@@ -65,8 +68,8 @@ if (isMobile) {
     table.dataTable thead .sorting_desc_disabled:before {
       bottom: .5em;
     }
-  .header_icon{
-    color: #ffffff;
+    .header_icon{
+        color: #ffffff;
         padding: 7px 0px !important;
         margin: 4px 6px 0px 0px;
         text-align: center;
@@ -75,31 +78,15 @@ if (isMobile) {
         width: 71px;
         border-radius: 10px;
         background-color: #4b9b4beb;height: 67px;
-  }
+    }
 </style>
-
-
-
-
-
-
 
 <!--responsive multilevel menu-->
 
 
 <link rel="stylesheet" type="text/css" href="{{ asset('public/css/component.css')}}" />
 
-
-
-
 <!--responsive multilevel menu-->
-
-
-
-
-
-
-
 
  <script>
 	function AddNew()
@@ -142,45 +129,44 @@ if (isMobile) {
 </script>
 
 <script>
-	function AddNew2()
-	{
-
-		  $("#addnew2").show();
-		 $("#addnew").hide();
-		 $("#addnew1").hide();
-		  $("#addnew3").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").hide();
-	}
+    function AddNew2()
+    {
+        $("#addnew2").show();
+        $("#addnew").hide();
+        $("#addnew1").hide();
+        $("#addnew3").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
+    }
 	function DivHide()
 	{
-		  $('#addnew1').hide();
-		   $("#addnew").hide();
-		   $("#addnew2").hide();
-		  $("#addnew3").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").hide();
+        $('#addnew1').hide();
+        $("#addnew").hide();
+        $("#addnew2").hide();
+        $("#addnew3").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
 	}
 </script>
 
 <script>
 	function AddNew3()
 	{
-		  $("#addnew3").show();
-		 $("#addnew").hide();
-		 $("#addnew1").hide();
-		  $("#addnew2").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").hide();
+        $("#addnew3").show();
+        $("#addnew").hide();
+        $("#addnew1").hide();
+        $("#addnew2").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
 	}
 	function DivHide()
 	{
-		  $('#addnew1').hide();
-		   $("#addnew").hide();
-		   $("#addnew2").hide();
-		  $("#addnew3").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").hide();
+        $('#addnew1').hide();
+        $("#addnew").hide();
+        $("#addnew2").hide();
+        $("#addnew3").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
 	}
 </script>
 
@@ -188,42 +174,42 @@ if (isMobile) {
 <script>
 	function AddNew4()
 	{
-		 $("#addnew3").hide();
-		 $("#addnew").hide();
-		 $("#addnew1").hide();
-		  $("#addnew2").hide();
-		  $("#addnew4").show();
-		   $("#addnew5").hide();
+        $("#addnew3").hide();
+        $("#addnew").hide();
+        $("#addnew1").hide();
+        $("#addnew2").hide();
+        $("#addnew4").show();
+        $("#addnew5").hide();
 	}
 	function DivHide()
 	{
-		  $('#addnew1').hide();
-		   $("#addnew").hide();
-		   $("#addnew2").hide();
-		  $("#addnew3").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").hide();
+        $('#addnew1').hide();
+        $("#addnew").hide();
+        $("#addnew2").hide();
+        $("#addnew3").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
 	}
 </script>
 
 <script>
 	function AddNew5()
 	{
-		  $("#addnew3").hide();
-		 $("#addnew").hide();
-		 $("#addnew1").hide();
-		  $("#addnew2").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").show();
+        $("#addnew3").hide();
+        $("#addnew").hide();
+        $("#addnew1").hide();
+        $("#addnew2").hide();
+        $("#addnew4").hide();
+        $("#addnew5").show();
 	}
 	function DivHide()
 	{
-		  $('#addnew1').hide();
-		   $("#addnew").hide();
-		   $("#addnew2").hide();
-		  $("#addnew3").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").hide();
+        $('#addnew1').hide();
+        $("#addnew").hide();
+        $("#addnew2").hide();
+        $("#addnew3").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
 	}
 </script>
 
@@ -231,36 +217,32 @@ if (isMobile) {
 <script>
 	function AddNew6()
 	{
-		  $("#addnew3").hide();
-		 $("#addnew").hide();
-		 $("#addnew1").hide();
-		  $("#addnew2").hide();
-		  $("#addnew4").hide();
-		   $("#addnew5").hide();
-		    $("#addnew6").show();
+        $("#addnew3").hide();
+        $("#addnew").hide();
+        $("#addnew1").hide();
+        $("#addnew2").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
+        $("#addnew6").show();
 	}
 	function DivHide()
 	{
-		   $('#addnew1').hide();
-		   $("#addnew").hide();
-		   $("#addnew2").hide();
-		   $("#addnew3").hide();
-		   $("#addnew4").hide();
-		   $("#addnew5").hide();
-		   $("#addnew6").hide();
+        $('#addnew1').hide();
+        $("#addnew").hide();
+        $("#addnew2").hide();
+        $("#addnew3").hide();
+        $("#addnew4").hide();
+        $("#addnew5").hide();
+        $("#addnew6").hide();
 	}
 </script>
 
  <!-- Owl Stylesheets -->
 
-  <link rel="stylesheet" href="{!! asset('public/owlcarousel/assets/owl.carousel.min.css') !!}">
-    <link rel="stylesheet" href="{!! asset('public/owlcarousel/assets/owl.theme.default.min.css') !!}">
+<link rel="stylesheet" href="{!! asset('public/owlcarousel/assets/owl.carousel.min.css') !!}">
+<link rel="stylesheet" href="{!! asset('public/owlcarousel/assets/owl.theme.default.min.css') !!}">
 
-
-
-  <!-- Owl Stylesheets -->
-
-
+<!-- Owl Stylesheets -->
 
 <!--scroll effect-->
 <!-- <link rel="stylesheet" href="css/style.css">-->
@@ -295,24 +277,6 @@ if (isMobile) {
 <link rel="stylesheet" type="text/css" href="{!! asset('public/source/jquery.fancybox8cbb.css?v=2.1.5') !!}" media="screen" />
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  <script>
 function isNumberKey(evt)
 {
@@ -326,19 +290,10 @@ function isNumberKey(evt)
 		return true;
 	}
 }
-
 </script>
 
 
-
-
-
-
-
 <link rel="stylesheet" href="{!! asset('public/css/style.css') !!}">
-
-
-
 
 <script>
 function get_center()
@@ -347,7 +302,6 @@ function get_center()
 	if(center_Id=='')
 	{
 		alert('Please Select Lab Location');
-
 	}
 	else
 	{
@@ -357,13 +311,11 @@ function get_center()
 </script>
 
 <script>
-function resget_center()
-{
-	var center_Id=$('#res_center').val();
-	window.location.href='index.html?center_Id='+center_Id;
-
-}
-
+    function resget_center()
+    {
+    	var center_Id = $('#res_center').val();
+    	window.location.href='index.html?center_Id='+center_Id;
+    }
 </script>
 
 
@@ -380,32 +332,28 @@ function resget_center()
 
 <style>
 #nav li a {
-/*padding: 13px 30px !important;*/
-padding: 13px 19px !important;
+    /*padding: 13px 30px !important;*/
+    padding: 13px 19px !important;
 }
 </style>
 
 
 <script>
 
-function GetOptionList()
-{
-	var a=$('#search-box1').val();
-
-
-		$.ajax({
-		type: "POST",
-		url: "test_search.php",
-		data:'keyword='+a,
-		success: function(data){
-			//alert(data);
-			$("#suggesstion-box").show();
-			$("#suggesstion-box").html(data);
-
-		}
-		});
-
-}
+    function GetOptionList()
+    {
+        var a=$('#search-box1').val();
+        $.ajax({
+            type: "POST",
+            url: "test_search.php",
+            data:'keyword='+a,
+            success: function(data){
+                //alert(data);
+                $("#suggesstion-box").show();
+                $("#suggesstion-box").html(data);
+            }
+        });
+    }
 
 /*$(document).ready(function(){
 	$("#search-box1").keyup(function(){
@@ -427,92 +375,81 @@ function GetOptionList()
 	});
 */
 
-function selectCountry(val) {
-$("#search-box1").val(val);
-$("#suggesstion-box").hide();
-}
+    function selectCountry(val) {
+        $("#search-box1").val(val);
+        $("#suggesstion-box").hide();
+    }
 
 </script>
 
 <script>
-function get_search()
-{
-
-/*var c= 'd e'
-var query= '?='+encodeURIComponent(c);
-var uri= 'http://www.example.com/script?query='+encodeURIComponent(query);
-*/
-
-	var search_home=$('#search-box1').val();
-	if(search_home=='')
-	{
-		//alert('Please Select Test Name');
-	}
-	else
-	{
-		window.location.href='find-test7407.html?search_item='+encodeURIComponent(search_home);
-	}
-}
-
+    function get_search()
+    {
+    	var search_home=$('#search-box1').val();
+    	if(search_home=='')
+    	{
+    		//alert('Please Select Test Name');
+    	}
+    	else
+    	{
+    		window.location.href='find-test7407.html?search_item='+encodeURIComponent(search_home);
+    	}
+    }
 </script>
 <script>
-function ShowMap(map_location)
-{
-    if(map_location=="west_bengal")
-	{
-	  $('#west_bengal').show();
-	  $('#delhi').hide();
-	  $('#bihar').hide();
-
-	  $("#wb_btn").addClass("tab_css_select");
-
-	  $("#wb_btn").removeClass("tab_css");
-	  $("#delhi_btn").removeClass("tab_css_select");
-	  $("#bihar_btn").removeClass("tab_css_select");
-
-
-	  $("#delhi_btn").addClass("tab_css");
-	  $("#bihar_btn").addClass("tab_css");
-	  document.getElementById('wbmap').src += '';
-	}
-	else if(map_location=="delhi")
+    function ShowMap(map_location)
     {
-	   $('#west_bengal').hide();
-	   $('#delhi').show();
-	   $('#bihar').hide();
+        if(map_location=="west_bengal")
+    	{
+            $('#west_bengal').show();
+            $('#delhi').hide();
+            $('#bihar').hide();
 
-	   $("#delhi_btn").addClass("tab_css_select");
+            $("#wb_btn").addClass("tab_css_select");
 
-	   $("#delhi_btn").removeClass("tab_css");
-	   $("#wb_btn").removeClass("tab_css_select");
-	   $("#bihar_btn").removeClass("tab_css_select");
+            $("#wb_btn").removeClass("tab_css");
+            $("#delhi_btn").removeClass("tab_css_select");
+            $("#bihar_btn").removeClass("tab_css_select");
 
-	   $("#wb_btn").addClass("tab_css");
-	   $("#bihar_btn").addClass("tab_css");
+            $("#delhi_btn").addClass("tab_css");
+            $("#bihar_btn").addClass("tab_css");
 
-	   document.getElementById('delhimap').src += '';
+            document.getElementById('wbmap').src += '';
+    	}
+    	else if(map_location=="delhi")
+        {
+            $('#west_bengal').hide();
+            $('#delhi').show();
+            $('#bihar').hide();
 
-	}
-	else if(map_location=="bihar")
-    {
-	   $('#west_bengal').hide();
-	   $('#delhi').hide();
-	   $('#bihar').show();
+            $("#delhi_btn").addClass("tab_css_select");
 
-	    $("#bihar_btn").addClass("tab_css_select");
+            $("#delhi_btn").removeClass("tab_css");
+            $("#wb_btn").removeClass("tab_css_select");
+            $("#bihar_btn").removeClass("tab_css_select");
 
-		 $("#bihar_btn").removeClass("tab_css");
-	     $("#wb_btn").removeClass("tab_css_select");
-		 $("#delhi_btn").removeClass("tab_css_select");
+            $("#wb_btn").addClass("tab_css");
+            $("#bihar_btn").addClass("tab_css");
 
-		 $("#wb_btn").addClass("tab_css");
-	     $("#delhi_btn").addClass("tab_css");
+            document.getElementById('delhimap').src += '';
+    	}
+    	else if(map_location=="bihar")
+        {
+            $('#west_bengal').hide();
+            $('#delhi').hide();
+            $('#bihar').show();
 
-		  document.getElementById('biharmap').src += '';
+            $("#bihar_btn").addClass("tab_css_select");
 
-	}
+            $("#bihar_btn").removeClass("tab_css");
+            $("#wb_btn").removeClass("tab_css_select");
+            $("#delhi_btn").removeClass("tab_css_select");
 
-}
+            $("#wb_btn").addClass("tab_css");
+            $("#delhi_btn").addClass("tab_css");
+            document.getElementById('biharmap').src += '';
+    	}
+    }
 </script>
 
 
@@ -520,18 +457,18 @@ function ShowMap(map_location)
 <style>
 
 .icon_subheading select{
-width: 100%;
-height: 40px;
-padding-left:10px;
--webkit-appearance: none;
--moz-appearance: none;
-appearance: none;
-text-indent: 0.01px;
-text-overflow: '';
-background: transparent url(images/select-icon.png) no-repeat scroll 97% 17px;
-font-size: 15px;
-font-family: 'Lato', sans-serif;
-color:#333333;
+    width: 100%;
+    height: 40px;
+    padding-left:10px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    text-indent: 0.01px;
+    text-overflow: '';
+    background: transparent url(images/select-icon.png) no-repeat scroll 97% 17px;
+    font-size: 15px;
+    font-family: 'Lato', sans-serif;
+    color:#333333;
 }
 
 }
@@ -642,8 +579,9 @@ display:none;
   fbq('init', '556624911842874');
   fbq('track', 'PageView');
 </script>
-<noscript><img height="1" width="1" style="display:none" alt="" src="{{$site_infos->facebook}}"
-/></noscript>
+<noscript>
+    <img height="1" width="1" style="display:none" alt="" src="{{$site_infos->facebook}}"/>
+</noscript>
 <!-- End Facebook Pixel Code -->
 
 </head>
